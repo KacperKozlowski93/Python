b9dccbaad5270fea7656ebd0c5e634f348426650
KacperKozlowski93 <81717255+KacperKozlowski93@users.noreply.github.com>
2026-06-09 20:45:57 +0200

    Delete 0.1. IC1.1 -- Type Coercion Pitfall.py

--- 0.1. IC1.1 -- Type Coercion Pitfall.py
+++
@@ -1,8 +0,0 @@
-a = "12"
-b = "5"
-total = a + b
-
-print(int(a) + int(b))
-
-# Zachodziła konkatenacja czyli łączenie Stringów i powstawał ciąg znaków zamiast sumy liczb
-#Należało zrobić konwersje typów ze String na int
