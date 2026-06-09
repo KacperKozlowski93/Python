5666e0673d8af19a8eaeb79a4db6ab0fdaeae7ec
KacperKozlowski93 <81717255+KacperKozlowski93@users.noreply.github.com>
2026-06-09 20:46:30 +0200

    Delete 0.1. IC1.7 -- Reading From Input With Validation.py

--- 0.1. IC1.7 -- Reading From Input With Validation.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        temperatura_w_stopniach_celsjusza = float(input("Podaj temperature w stopniach Celsjusza: "))
-        temperatura_w_stopniach_fahrenheita = (temperatura_w_stopniach_celsjusza * 9 / 5) + 32
-        break
-    except ValueError:
-        print("Złe dane, spróbuj jeszcze raz")
-
-print(f"Temperatura w stopniach fahrenheita wynosi: {temperatura_w_stopniach_fahrenheita:.1f}")
